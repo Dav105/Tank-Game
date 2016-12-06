9563aae93ebe2bff1a9e3a44f1243b8a4ca5dde8
AAAntzzz <anthony.lines@mail.bcu.ac.uk>
2016-12-06 16:03:17 +0000

    Initial Commit
    added main files

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public Rigidbody prefab;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 carMove = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            carMove = new Vector3(0, 5, 0) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            carMove = new Vector3(0, -5, 0) * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Fire();
+        }
+
+
+        transform.Translate(carMove);
+    }
+
+    void Fire()
+    {
+        Rigidbody lprefab = Instantiate(prefab, new Vector3(transform.position.x + 1f, transform.position.y, 0), gameObject.transform.rotation) as Rigidbody;
+
+
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Shooting : MonoBehaviour {
+    public Text text2;
+    int score1;
+	// Use this for initialization
+	void Start () {
+        text2 = GameObject.Find("P1Text").GetComponent<Text>();
+        text2.text = "Score: ";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(-25 * Time.deltaTime, 0, 0);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player1")
+            Destroy(gameObject);
+            score1 += 100;
+            text2.text = "Score: " + score1;
+    }
+}
